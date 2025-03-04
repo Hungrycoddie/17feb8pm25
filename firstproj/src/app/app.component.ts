c2794070f7ac34f8fb4e6e817081204a3ef1405f
Arun raj <106869558+Hungrycoddie@users.noreply.github.com>
2025-03-04 20:28:04 +0530

    update

--- firstproj/src/app/app.component.ts
+++ firstproj/src/app/app.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent} from './header/header.component';
-import { FooterComponent } from "./footer/footer.component";
-import { coreUImodule } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-
-
-  
-export class AppComponent {
-
-}
+export class AppComponent {}
